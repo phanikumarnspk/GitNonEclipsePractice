b6df3b4cac52a7c5f9f18c8f97faa33637f1e97f
bvnBhuvana <125237984+bvnBhuvana@users.noreply.github.com>
2023-06-07 16:02:20 +0530

    Update bvnOne.java

---
+++ bvnOne.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Hi{
+  public static void main(String[] args){
+    Scanner s=new Scanner(System.in);
+    int num=s.nextInt();
+    String str=s.next();
+    System.out.println(str+"  *****  "+num);
+  }
+}
